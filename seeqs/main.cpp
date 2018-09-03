565a6c64e00191256a5f77ab2978648a0e6f58b0
Tim Gallant <tgallant@attn.com>
2018-09-03 16:19:58 -0700

    Initial Commit

---
+++ seeqs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  seeqs
+//
+//  Created by Tim Gallant on 9/3/18.
+//  Copyright © 2018 Tim Gallant. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
